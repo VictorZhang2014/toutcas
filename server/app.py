0e3551236160db9731a6946caa30d4a7dad76d40
Victor <victorjon@163.com>
2025-12-01 18:36:31 +0800

    add: deploy server to vercel

--- server/app.py
+++ server/app.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 app = Flask(__name__) 
 
+@app.route("/")
+def home():
+    return "I'm ToutCas, your personal AI assistant."
+
 for bp in (webpage_content.webpage_content_bp,
            text_analyser.text_analyser_bp, 
            pdf_analyser.pdf_analyzer_bp,
@@ -13,5 +17,5 @@ for bp in (webpage_content.webpage_content_bp,
            ):
     app.register_blueprint(bp, url_prefix="/")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=20250, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=20250, debug=True)
